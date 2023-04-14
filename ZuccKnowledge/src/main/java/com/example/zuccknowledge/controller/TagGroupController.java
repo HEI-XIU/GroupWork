528d34110796da01c701f1f4d14890355c7760a8
liucaixia49 <87164078+liucaixia49@users.noreply.github.com>
2023-04-14 18:27:35 +0800

    Update TagGroupController.java

--- ZuccKnowledge/src/main/java/com/example/zuccknowledge/controller/TagGroupController.java
+++ ZuccKnowledge/src/main/java/com/example/zuccknowledge/controller/TagGroupController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/tagGroup")
+@RequestMapping("/taggroup")
 public class TagGroupController {
     @Autowired
     private GroupRepository tgRepository;
