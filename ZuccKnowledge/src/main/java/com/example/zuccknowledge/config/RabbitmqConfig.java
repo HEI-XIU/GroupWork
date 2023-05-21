2405ce313eda04c1292564ea6b3a88b5d17b447f
W0318 <115613583+W0318@users.noreply.github.com>
2023-05-21 14:35:17 +0800

    rabbitmq-readRecord

---
+++ ZuccKnowledge/src/main/java/com/example/zuccknowledge/config/RabbitmqConfig.java
@@ -0,0 +1,29 @@
+package com.example.zuccknowledge.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+    //创建队列
+    @Bean
+    public Queue kRankQueue() {
+        return new Queue("KRankQueue");
+    }
+
+    // 创建交换机
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange("routingExchange");
+    }
+
+    //绑定队列
+    @Bean
+    Binding bingingKRankQueue(Queue kRankQueue, DirectExchange directExchange) {
+        return BindingBuilder.bind(kRankQueue).to(directExchange).with("kRank");
+    }
+}
